7aa002d883c39c0a93779b92abc31ec1dbaf39cb
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-03-23 19:51:44 +0900

    메뉴 바에서 쓰일 국가별 장르 리스트

---
+++ src/GenresByNation.js
@@ -0,0 +1,5 @@
+export const krGenres = ["순정만화", "소년만화", "성인만화", "기획도서", "만화잡지"];
+
+export const jpGenres = ["순정만화", "소년만화", "성인만화", "기획도서"];
+
+export const usGenres = ["DC 코믹스마블", "코믹스리터러리", "그래픽노블"];
